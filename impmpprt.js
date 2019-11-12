3b074be492cf16a034b47a7aa2ed0512a7e5aeef
NisrineJednane <34342007+NisrineJednane@users.noreply.github.com>
2019-11-12 01:02:13 +0100

    Update impmpprt.js

--- impmpprt.js
+++ impmpprt.js
@@ -4,6 +4,14 @@ Array.prototype.myMap = function(callback){
 
   var newArray = [];
   this.forEach(x=> newArray.push(callback(x)));
+  
+  /*second method using a for loop:
+  
+  for (let x=0; x<this.length; x++) {
+    newArray.push(callback(this[x]));
+  }
+  */
+  
   return newArray;
 
 };
